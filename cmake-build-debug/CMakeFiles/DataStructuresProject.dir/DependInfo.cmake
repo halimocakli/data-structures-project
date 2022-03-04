bcf37506209311b93573c55eb6cf8a48a0ad465d
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-03-05 01:56:07 +0300

    Update DependInfo.cmake

--- cmake-build-debug/CMakeFiles/DataStructuresProject.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/DataStructuresProject.dir/DependInfo.cmake
@@ -4,20 +4,11 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/halim/OneDrive/Desktop/DataStructures/DataStructuresProject/main.c" "C:/Users/halim/OneDrive/Desktop/DataStructures/DataStructuresProject/cmake-build-debug/CMakeFiles/DataStructuresProject.dir/main.c.obj"
-  )
-set(CMAKE_C_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/can/Documents/GitHub/DataStructuresProject/main.c" "CMakeFiles/DataStructuresProject.dir/main.c.o" "gcc" "CMakeFiles/DataStructuresProject.dir/main.c.o.d"
   )
 
 # Targets to which this target links.
